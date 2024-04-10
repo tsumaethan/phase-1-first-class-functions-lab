2bdf677f1c4d7cb964237f416be7292168d3e9a0
tsumaethan <tsumaethan20@gmail.com>
2024-04-10 10:34:18 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  const createFareMultiplier = function(multiplier) {
+    return function(value) {
+      return value * multiplier;
+    };
+  };
+  
+  const fareDoubler = createFareMultiplier(2);
+  
+  const fareTripler = createFareMultiplier(3);
+  
+  const selectDifferentDrivers = function(drivers, fn) {
+    return fn(drivers);
+  };
+  
+  module.exports = {
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+    selectingDrivers,
+    createFareMultiplier,
+    fareDoubler,
+    fareTripler,
+    selectDifferentDrivers
+  };
+  
